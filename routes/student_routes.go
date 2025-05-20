50ce9c623be2e3c33195c0929ae737cdc3e2e842
SachinthaNimesh <sanisenevirathna@gmail.com>
2025-05-20 14:33:13 +0530

    refactor: update API paths for moods and students to use singular form and enhance route organization

--- routes/student_routes.go
+++ routes/student_routes.go
@@ -9,16 +9,16 @@ import (
 func RegisterStudentRoutes(router *mux.Router) {
 	router.HandleFunc("/students", controllers.GetStudents).Methods("GET")
 	// router.HandleFunc("/students", controllers.CreateStudent).Methods("POST")
-	router.HandleFunc("/students", controllers.GetStudent).Methods("GET")
-	router.HandleFunc("/students", controllers.UpdateStudent).Methods("PUT")
-	router.HandleFunc("/students", controllers.DeleteStudent).Methods("DELETE")
+	router.HandleFunc("/student", controllers.GetStudent).Methods("GET")
+	router.HandleFunc("/student", controllers.UpdateStudent).Methods("PUT")
+	router.HandleFunc("/student", controllers.DeleteStudent).Methods("DELETE")
 
 	// Add attendance routes
 	router.HandleFunc("/attendance", controllers.PostAttendance).Methods("POST")
 
 	// Add mood routes
-	router.HandleFunc("/moods", controllers.CreateMood).Methods("POST")
-	router.HandleFunc("/moods", controllers.GetMoods).Methods("GET")
+	router.HandleFunc("/mood", controllers.CreateMood).Methods("POST")
+	router.HandleFunc("/mood", controllers.GetMoods).Methods("GET")
 
 	// Add card routes
 	router.HandleFunc("/dashboard", controllers.GetStudentDetails).Methods("GET")
