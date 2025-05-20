6bd76530b88203e16d5ddf28dff086d0d9204290
SachinthaNimesh <sanisenevirathna@gmail.com>
2025-05-20 15:42:48 +0530

    CORS policy updated

--- main.go
+++ main.go
@@ -25,10 +25,18 @@ func main() {
 	log.Println("Database migrated")
 
 	// Define router
-	r := mux.NewRouter()
+	router := mux.NewRouter()
+
+	// CORS Setup - Set allowed origins to Choreo API Gateway
+	corsMiddleware := handlers.CORS(
+		handlers.AllowedHeaders([]string{"Content-Type", "Authorization", "Student-ID"}), // Ensure "Student-ID" is included
+		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"}),
+		handlers.AllowedOrigins([]string{"https://87abc270-1269-4d98-8dad-e53781a1ae52.e1-us-east-azure.choreoapps.dev"}), // Replace "*" with the actual origin in production
+	)
+	router.Use(corsMiddleware)
 
 	// Register API routes
-	routes.RegisterStudentRoutes(r)
+	routes.RegisterStudentRoutes(router)
 
 	// // Route to handle location updates
 	// r.HandleFunc("/location", controllers.HandleLocationUpdate).Methods("POST")
@@ -39,16 +47,9 @@ func main() {
 	// // Serve static files for the React web app
 	// r.PathPrefix("/").Handler(http.FileServer(http.Dir("./web")))
 
-	// CORS Setup - Set allowed origins to Choreo API Gateway
-	corsMiddleware := handlers.CORS(
-		handlers.AllowedHeaders([]string{"Content-Type", "Authorization", "Student-ID"}), // Ensure "Student-ID" is included
-		handlers.AllowedMethods([]string{"GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS"}),
-		handlers.AllowedOrigins([]string{"https://87abc270-1269-4d98-8dad-e53781a1ae52.e1-us-east-azure.choreoapps.dev"}), // Replace "*" with the actual origin in production
-	)
-
 	// Start the server
 	log.Println("Server started on port", port)
-	if err := http.ListenAndServe(":"+port, corsMiddleware(r)); err != nil {
+	if err := http.ListenAndServe(":"+port, router); err != nil {
 		log.Fatalf("Could not start server: %s\n", err)
 	}
 }
