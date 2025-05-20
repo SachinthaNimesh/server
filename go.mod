b470b1b9245e8c417f7b06127d774e72d93f32f8
SachinthaNimesh <sanisenevirathna@gmail.com>
2025-05-20 14:57:39 +0530

    refactor: update student and mood routes to use consistent naming and improve organization

--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 
 require (
 	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/go-openapi/jsonpointer v0.21.1 // indirect
 	github.com/go-openapi/jsonreference v0.21.0 // indirect
@@ -20,6 +21,8 @@ require (
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.9.0 // indirect
 	github.com/mattn/go-sqlite3 v1.14.15 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/testify v1.10.0 // indirect
 	golang.org/x/tools v0.31.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- routes/student_routes.go
+++ routes/student_routes.go
@@ -7,18 +7,19 @@ import (
 )
 
 func RegisterStudentRoutes(router *mux.Router) {
-	router.HandleFunc("/students", controllers.GetStudents).Methods("GET")
-	// router.HandleFunc("/students", controllers.CreateStudent).Methods("POST")
-	router.HandleFunc("/student", controllers.GetStudent).Methods("GET")
-	router.HandleFunc("/student", controllers.UpdateStudent).Methods("PUT")
-	router.HandleFunc("/student", controllers.DeleteStudent).Methods("DELETE")
+	router.HandleFunc("/get-students", controllers.GetStudents).Methods("GET")
+	router.HandleFunc("/post-student", controllers.CreateStudent).Methods("POST")
+
+	router.HandleFunc("/get-student", controllers.GetStudent).Methods("GET")
+
+	router.HandleFunc("/delete-student", controllers.DeleteStudent).Methods("DELETE")
 
 	// Add attendance routes
 	router.HandleFunc("/attendance", controllers.PostAttendance).Methods("POST")
 
 	// Add mood routes
-	router.HandleFunc("/mood", controllers.CreateMood).Methods("POST")
-	router.HandleFunc("/mood", controllers.GetMoods).Methods("GET")
+	router.HandleFunc("/post-mood", controllers.CreateMood).Methods("POST")
+	router.HandleFunc("/get-mood", controllers.GetMoods).Methods("GET")
 
 	// Add card routes
 	router.HandleFunc("/dashboard", controllers.GetStudentDetails).Methods("GET")
