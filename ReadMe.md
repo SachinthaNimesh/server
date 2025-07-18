3b747c03ed9dae7da3552f222f572e69f2fce11d
Sachintha Dissanayake <124299595+SachinthaNimesh@users.noreply.github.com>
2025-07-18 13:44:06 +0530

    Backend connected

--- ReadMe.md
+++
@@ -1,3 +0,0 @@
-Change Environment Variables when migrating domains
-Change Azure URLs (For frontend)
-In config file set API_URL to correct URL
--- models/supervisor.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Supervisor struct {
-	SupervisorID  int    `json:"supervisor_id"`
-	FirstName     string `json:"first_name"`
-	LastName      string `json:"last_name"`
-	EmailAddress  string `json:"email_address"`
-	ContactNumber string `json:"contact_number"`
-}
